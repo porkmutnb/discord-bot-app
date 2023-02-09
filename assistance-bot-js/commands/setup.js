2a01437eeaef619fd8067b6930c49eac775b29d3
cherMew <kmutnbenetpor@gmail.com>
2023-02-09 20:15:47 +0700

    link thumnail assistance-bot

--- assistance-bot-js/commands/setup.js
+++ assistance-bot-js/commands/setup.js
@@ -23,7 +23,7 @@ module.exports = {
                             .setColor(0xC995C1)
                             .setTitle('คำสั่งของดิชั้นนะคะ')
                             .setDescription('Command for Assistance')
-                            .setThumbnail(`https://scontent.fbkk8-2.fna.fbcdn.net/v/t39.30808-6/302054065_625054718989411_2467317685068967605_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHiZa-EYXRJGRn5txfNoA8j3mHiALYAztfeYeIAtgDO18JG0qXepRYB25_dT-BWhxbNbAO9NkXcs3vgqdOTx-jh&_nc_ohc=dnlnQmXHJLYAX8eP7CR&_nc_ht=scontent.fbkk8-2.fna&oh=00_AfA158qPGt1adpfH-6f4FsJq_Qqkgdsr8BKro0kX7riIbw&oe=63DBFEEE`)
+                            .setThumbnail(`https://scontent.fbkk8-2.fna.fbcdn.net/v/t39.30808-6/302054065_625054718989411_2467317685068967605_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=uv15l4Z6KlwAX-eccvc&_nc_ht=scontent.fbkk8-2.fna&oh=00_AfCL2xb7PWvC47l4LsiymMEvzLuOt00siFVcWenCRedvqg&oe=63E9D6AE`)
                             .addFields(
                                 { name: '/pupe', value: 'คุยกับดิชั้น', inline: true },
                                 { name: '/role', value: 'ต้องการ Role', inline: true },
