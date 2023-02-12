c6bb31e6376b60c90c54841c98f4275b666a7700
cherMew <kmutnbenetpor@gmail.com>
2023-02-12 19:53:15 +0700

    no message

--- information-bot-js/events/ready.js
+++ information-bot-js/events/ready.js
@@ -42,7 +42,7 @@ module.exports = {
         setInterval(async () => {
             updateLatestVideoBNK(client);
             updateLatestVideoCGM(client);
-            renderSummaryCovid19();
+            renderSummaryCovid19(client);
         }, 60 * 60 * 1000);
         /***** Every minute *****/
         // setInterval(async () => {
