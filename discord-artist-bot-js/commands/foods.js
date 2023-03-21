41a105e3c22bcf20c74a46259f64fc57a2823196
cherMew <kmutnbenetpor@gmail.com>
2023-03-21 13:15:54 +0700

    add feature foods on artist-bot

--- discord-artist-bot-js/commands/foods.js
+++ discord-artist-bot-js/commands/foods.js
@@ -1,16 +1,25 @@
+const { getDataFoodsList } = require('../function/retriveDataGoogleSheet');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('foods')
-		.setDescription('Englisg to Thai translate.')
-        .addStringOption(option =>
-            option.setName('food')
-                .setDescription('text input')
-                .setRequired(true)),
+		.setDescription('what to eat'),
 	async execute(interaction) {
-        const food = interaction.options.getString('food')
-
+        const foodsList = await getDataFoodsList();
+        let resMsg = `กระเพราหมูสับไข่ดาว`
+        if(foodsList.length>0) {
+            let indexRan = Math.floor(Math.random() * foodsList.length);
+            resMsg = `${foodsList[indexRan]}`
+        }
+        const embed = new EmbedBuilder()
+                            .setColor("#C995C1")
+                            .setTitle(`หนูรับทราบค่ะ`)
+                            .setDescription(`ลองเมนูนี้ดูสิ ${resMsg}`)
+                            .setTimestamp()
+                            .setFooter({ text: 'Powerd be cherMew', iconURL: `https://cdn.discordapp.com/icons/${interaction.guild.id}/${interaction.guild.icon}.webp` });
+        await interaction.followUp({embeds: [embed]});
+        setTimeout(() => interaction.deleteReply(), 3000);
     },
 };
---
+++ discord-artist-bot-js/function/retriveDataGoogleSheet.js
@@ -0,0 +1,30 @@
+const { google } = require('googleapis');
+require('dotenv').config();
+
+const auth = new google.auth.GoogleAuth({
+    credentials: require('../credentials.json'),
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+});
+
+const sheets = google.sheets({ version: 'v4', auth });
+const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+
+module.exports.getDataFoodsList = async () => {
+    let itemList = [];
+    try {
+        const range = `FOODS!A2:A1000`;
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range,
+        });
+        const rows = response.data.values;
+        await rows.forEach(element => {
+            if(element[0]!=undefined && element[0]!=``) {
+                itemList.push(element[0]);
+            }
+        })
+    } catch (error) {
+        console.error(error);
+    }
+    return itemList;
+}
